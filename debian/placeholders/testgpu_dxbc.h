a6705d11677f210824ba47bf16c3070ab0081ae3
lhdjply <lhdjply@126.com>
2025-02-17 16:20:27 +0800

    feat: update libsdl3 to 3.2.4+ds-2
    Signed-off-by: lhdjply <lhdjply@126.com>

---
+++ debian/placeholders/testgpu_dxbc.h
@@ -0,0 +1,3 @@
+/* Same API as upstream's generated header */
+const unsigned char D3D11_CubeVert[] = { 0 };
+const unsigned char D3D11_CubeFrag[] = { 0 };
---
+++ debian/tests/cmake-example/hello.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2019 Collabora Ltd.
+ * SPDX-License-Identifier: Zlib
+ * (see "zlib/libpng" in debian/copyright)
+ */
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+int
+main (int argc, char **argv)
+{
+  if (!SDL_Init(0)) {
+      SDL_Log("SDL_Init: %s", SDL_GetError());
+      return 1;
+  }
+
+  SDL_Log("Hello, world!");
+  SDL_Quit();
+  return 0;
+}
